31d1598371b3115ad5b225877687f44c30ede318
Sanyam Madan <sanyammadan3@gmail.com>
2026-07-14 01:52:20 +0530

    react app completed maybe 1:52am

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -18,7 +18,7 @@ export default function Texthtmlform(props) {
     setText("");
     props.showAlert("All Cleared", "success")
   }
-  
+
   const speak = () => {
     let msg = new SpeechSynthesisUtterance();
     msg.text = text;
@@ -36,77 +36,78 @@ export default function Texthtmlform(props) {
   }
   const handleCopy = () => {
     navigator.clipboard.writeText(text);
+    document.getSelection().removeAllRanges();
     props.showAlert("copied to clipboard ", "success")
   }
   const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "))
     props.showAlert("removed all extra spaces ", "success")
-}
+  }
 
   const [text, setText] = useState("enter here");
   return (
     <>
-      <div className="container " style={{ color: props.mode== 'light'? 'black': 'white'}} >
+      <div className="container " style={{ color: props.mode == 'light' ? 'black' : 'white' }} >
         <h2>{props.Heading}</h2>
         {/* <h6 onClick= {BtnClicked}></h6> */}
         {/* <label htmlfor="MyBox" className="form-label"></label> */}
         <textarea
-          className="form-control"data-bs-theme= {props.mode}
+          className="form-control" data-bs-theme={props.mode}
           id="MyBox"
           rows="5"
           value={text}
           onChange={Changed}
         ></textarea>
-        <button className="btn btn-primary" onClick={BtnClicked1} id="btn1">
+        <button disabled={text.length===0} className="btn btn-primary" onClick={BtnClicked1} id="btn1">
           {" "}
           Convert to Upper Case{" "}
         </button>
-        <button className="btn btn-primary" onClick={BtnClicked2} id="btn2">
+        <button  disabled={text.length===0} className="btn btn-primary" onClick={BtnClicked2} id="btn2">
           {" "}
           Convert to Lower Case{" "}
         </button>
-        <button className="btn btn-primary" onClick={BtnClicked3} id="btn3">
+        <button  disabled={text.length===0} className="btn btn-primary" onClick={BtnClicked3} id="btn3">
           {" "}
           Clear All{" "}
         </button>
-        <button
+        <button disabled={text.length===0} 
           type="submit"
           onClick={speak}
           className="btn btn-warning mx-2 my-2"
         >
           Speak
         </button>
-        <button
+        <button disabled={text.length===0} 
           type="submit"
           className="btn btn-danger mx-2"
           onClick={cancelSpeech}
         >
           Stop Speach
         </button>
-        <button id="btn5" className="btn btn-dark" onClick={HandleReverse}>
+        <button  disabled={text.length===0} id="btn5" className="btn btn-dark" onClick={HandleReverse}>
           {" "}
           Reverse
         </button>
-        <button id="btn6" className="btn btn-dark" onClick={handleCopy}>
+        <button  disabled={text.length===0} id="btn6" className="btn btn-dark" onClick={handleCopy}>
           {" "}
-          Copy Text 
+          Copy Text
         </button>
-        <button id="btn7" className="btn btn-dark" onClick={handleExtraSpaces}>
+        <button  disabled={text.length===0} id="btn7" className="btn btn-dark" onClick={handleExtraSpaces}>
           {" "}
           Remove Extra Spaces
         </button>
       </div>
-      <div className="container my-3" style={{ color: props.mode== 'light'? 'black': 'white'}}   >
+      <div className="container my-3" style={{ color: props.mode == 'light' ? 'black' : 'white' }}   >
         <b>
           <h3>Your Text Summary </h3>
         </b>
         <p>
-          {text.split(" ").filter((element)=>{return element.length!==0}).length} Words and {text.length} characters
+          {text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Words and {text.length} characters
         </p>
-        <p>{0.008 * text.split(" ").length} Minutes needed to read this </p>
+        <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} Minutes needed to read this </p>
         <h2>Preview</h2>
-        <p>{text.length>0? text:" KUCH LIKH TOH KAM SE KAM"}</p>
+        <p>{text.length > 0 ? text : " KUCH LIKH TOH KAM SE KAM"}</p>
       </div>
     </>
   );
