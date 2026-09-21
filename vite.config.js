58d5c346334384cd51e437bafc7cf07916b6205b
Sanyam Madan <sanyammadan3@gmail.com>
2026-09-22 00:25:50 +0530

    fix: change base path to root for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/my-react-app/' : '/',
-}))
+  base: '/', // This ensures it correctly reads from the root directory on Vercel
+})
